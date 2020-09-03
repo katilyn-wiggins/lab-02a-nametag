219106624733e1893cb2c3e95dce8bc4b583831f
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2020-09-02 17:21:47 -0700

    secondary commit

--- javascript.js
+++ javascript.js
@@ -1,8 +1,30 @@
 const myName = document.getElementById('name')
 const myInput = document.getElementById('name-input')
-const myButton = document.getElementById('name-button')
+const myButton = document.getElementById('main')
+const main = document.getElementById('main')
 
 myButton.addEventListener('click', () => {
     const userInput = myInput.value;
     myName.textContent = userInput; 
-})
+});
+
+
+const color1 = document.getElementById('color-button1')
+const color2 = document.getElementById('color-button2')
+const color3 = document.getElementById('color-button3')
+
+color1.addEventListener('click', () => {
+    main.style.background = color1.value;
+
+});
+
+color2.addEventListener('click', () => {
+    main.style.background = color2.value;
+
+});
+
+color3.addEventListener('click', () => {
+    main.style.background = color3.value;
+
+});
+
